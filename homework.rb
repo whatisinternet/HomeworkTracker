1c98e1b7792c78affc1332dfde96054e4ab7d3d7
Whatisinternet <joshteeter@gmail.com>
2014-06-23 18:33:13 -0400

    2014-06-23T18-32-41-04-00

--- homework.rb
+++ homework.rb
@@ -54,6 +54,7 @@ class HomeWork
 			cd #{@@default_directory}#{project_name.to_s} &&
 			git checkout master &&
 			git merge #{current_branch} &&
+			git commit -m #{current_branch} &&
 			git tag -a v#{version.to_s} -m '#{current_branch}' &&
 			git branch -D #{current_branch} &&
 			git checkout -b #{new_branch_name}
