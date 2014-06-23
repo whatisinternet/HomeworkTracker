e5207f2bab643ae266d6ab3592aa4d7f65e1814f
Whatisinternet <joshteeter@gmail.com>
2014-06-23 12:11:03 -0400

    2014-06-23T11-48-03-04-00

--- homework.rb
+++ homework.rb
@@ -13,9 +13,17 @@ class HomeWork
 	@@setting_manager = Setting_Manager.new
 	@@default_directory = @@setting_manager.get_default_directory
 
+	def branch_generator(project_name)
+		dt = Time.now
+		branch_name = dt.to_time.iso8601.gsub(' ', '').gsub(':','-')
+		if project_name != ''
+					@@setting_manager.set_branch(project_name, branch_name)
+		end
+		branch_name
+	end
+
 	# Creates a homework assignment as a new project
 	def create(project_name)
-
 		new_branch_name = branch_generator(project_name)
 		if project_name.nil?
 			puts "Missing project name!"
@@ -24,19 +32,6 @@ class HomeWork
 			exec("mkdir #{@@default_directory}#{project_name.to_s} && cd #{@@default_directory}#{project_name.to_s} && git init && git checkout -b master && git commit --allow-empty -m 'first commit' && git checkout -b #{new_branch_name}")
 			system("cd #{@@default_directory}#{project_name.to_s}")
 			save(project_name)
-		end
-	end
-
-	# Reverts changes to a project
-	def undo(project_name)
-
-		current_branch = @@setting_manager.get_branch(project_name)
-		new_branch_name = branch_generator(project_name)
-
-		if project_name.nil?
-			puts "Missing project name!"
-		else
-			exec("cd #{@@default_directory}#{project_name.to_s} && git checkout master && git branch -D #{current_branch} && git checkout -b #{new_branch_name}")
 		end
 	end
 
@@ -71,13 +66,17 @@ class HomeWork
 		end
 	end
 
-	def branch_generator(project_name)
-		dt = Time.now
-		branch_name = dt.to_time.iso8601.gsub(' ', '').gsub(':','-')
-		if project_name != ''
-					@@setting_manager.set_branch(project_name, branch_name)
+	# Reverts changes to a project
+	def undo(project_name)
+
+		current_branch = @@setting_manager.get_branch(project_name)
+		new_branch_name = branch_generator(project_name)
+
+		if project_name.nil?
+			puts "Missing project name!"
+		else
+			exec("cd #{@@default_directory}#{project_name.to_s} && git checkout master && git branch -D #{current_branch} && git checkout -b #{new_branch_name}")
 		end
-		branch_name
 	end
 
 end #end class
