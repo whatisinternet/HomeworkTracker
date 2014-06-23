fe02a89d13046e43dae9afb2cc5e079f79afc13f
Whatisinternet <joshteeter@gmail.com>
2014-06-23 12:22:01 -0400

    2014-06-23T12-15-31-04-00

--- install.sh
+++ install.sh
@@ -4,12 +4,18 @@ echo 'Setup the directories'
 mkdir ~/projects
 mkdir ~/HomeworkTracker
 mkdir ~/HomeworkTracker/projects
+mkdir ~/HomeworkTracker/projects/Homework
 
 gem install listen
 gem install yaml
+
+_mydir="$(pwd)"
+cp _mydir ~/HomeworkTracker/projects/Homework
+
+ruby ~/HomeworkTracker/projects/Homework/main.rb start
 
 echo 'Creating aliases for Homework'
 echo "\nalias homework='ruby ~/HomeworkTracker/projects/Homework/main.rb \$1 \$2'">>~/.bash_aliases
 source ~/.bash_aliases
 clear
-echo "Setup complete!"
+echo "Setup complete! Restart your terminal and try a command like 'homework create [my project name]"
