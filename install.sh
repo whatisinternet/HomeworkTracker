9602b57f580e9a3b50f718a8e94ec39cc21f42c7
Whatisinternet <joshteeter@gmail.com>
2014-06-23 12:17:32 -0400

    2014-06-23T12-15-31-04-00

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ mkdir ~/projects
 mkdir ~/HomeworkTracker
 mkdir ~/HomeworkTracker/projects
 
+gem install listen
+gem install yaml
+
 echo 'Creating aliases for Homework'
 echo "\nalias homework='ruby ~/HomeworkTracker/projects/Homework/main.rb \$1 \$2'">>~/.bash_aliases
 source ~/.bash_aliases
