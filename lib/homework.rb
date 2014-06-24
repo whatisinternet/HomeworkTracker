7690f1f47dbcaea77dc2d232b1de0088c9c98d5a
Whatisinternet <joshteeter@gmail.com>
2014-06-24 01:54:39 -0400

    2014-06-24T01-49-03-04-00

--- lib/homework.rb
+++ lib/homework.rb
@@ -56,8 +56,7 @@ class HomeWork
 			git checkout master &&
 			git merge #{current_branch} &&
 			git branch -D #{current_branch} &&
-			git fetch origin master &&
-			git rebase master &&
+			git pull –rebase &&
 			git checkout -b #{new_branch_name}
 			")
 		end
