407f14a2ecc2edc177e1f84071bf2719f52d71eb
Whatisinternet <joshteeter@gmail.com>
2014-06-23 20:08:21 -0400

    2014-06-23T20-07-19-04-00

--- homework.rb
+++ homework.rb
@@ -70,7 +70,8 @@ class HomeWork
 			exec("
 			cd #{@@default_directory}#{project_name.to_s} &&
 			git add --all . &&
-			git commit -m #{current_branch}
+			git commit -m #{current_branch} &&
+			git tag #{current_branch}
 			")
 		end
 	end
